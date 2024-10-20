4599525d6f0624b353f9788c2b043d1641407d67
exoil <jakubtederko@outlook.com>
2024-10-20 09:31:13 +0200

    Update

--- src/RpgAssistant.Api/IoC/EndpointRegisters.cs
+++ src/RpgAssistant.Api/IoC/EndpointRegisters.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace RpgAssistant.Api.IoC;
 
 public static class EndpointRegisters
@@ -5,9 +7,9 @@ public static class EndpointRegisters
     public static void RegisterUtilitiesEndpoints(
         this WebApplication webApplication)
     {
-        string LocalFunction() => "RpgAssistant version 1.0.0";
         webApplication
-            .MapGet("/", LocalFunction)
+            .MapGet("/",  ([FromServices]IConfiguration configuration) =>
+                configuration.GetSection("AppVersion").Value)
             .Produces<string>();
     }
 }
