8d8b54c47792692db8e50a2041ce298e466dab18
exoil <jakubtederko@outlook.com>
2024-10-20 09:13:31 +0200

    add description for Result

--- src/RpgAssistant.Application/Models/Result.cs
+++ src/RpgAssistant.Application/Models/Result.cs
@@ -1,5 +1,10 @@
 namespace RpgAssistant.Application.Models;
 
+/// <summary>
+/// Result TError  is a readonly struct, that can be used to wrap the result
+/// of an operation that could potentially fail with an exception. It provides
+/// information about whether the operation was successful and the associated error message (if any).
+/// </summary>
 public readonly struct Result<TError>
     where TError : Exception
 {
@@ -12,11 +17,22 @@ public readonly struct Result<TError>
         Error = error;
         _isError = error is not null;
     }
-    
+
+    /// <summary>
+    /// Implicit conversion from TError type to Result TError. It enables you to
+    /// create a Result without having to explicitly instantiate a new Result.
+    /// </summary>
     public static implicit operator Result<TError>(TError error)
         => new(error);
 }
 
+
+/// <summary>
+/// Result TValue, TError is a readonly struct that can be used to wrap
+/// the result of an operation that could potentially fail with an exception and provides 
+/// the result of operation itself. It provides information about whether the operation
+/// was successful, the returned value, and the associated error message (if any).
+/// </summary>
 public readonly struct Result<TValue, TError>
     where TError : Exception
 {
@@ -50,8 +66,17 @@ public readonly struct Result<TValue, TError>
         Error = error;
     }
 
+    /// <summary>
+    /// Implicit conversion from TValue type to Result TValue, TError. It enables you to
+    /// create a Result without having to explicitly instantiate a new Result.
+    /// </summary>
     public static implicit operator Result<TValue, TError>(TValue value)
         => new(value);
+
+    /// <summary>
+    /// Implicit conversion from TError type to Result TValue, TError. It enables you to
+    /// create a Result without having to explicitly instantiate a new Result.
+    /// </summary>
     public static implicit operator Result<TValue, TError>(TError error)
         => new(error);
-}
+} 
