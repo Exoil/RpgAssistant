b60281a71c87a7e2ab12cc4d77c4cf82d8090a25
exoil <jakubtederko@outlook.com>
2024-10-21 22:15:26 +0200

    Update BaseValueObject

--- src/RpgAssistant.Domain/Models/BaseValueObject.cs
+++ src/RpgAssistant.Domain/Models/BaseValueObject.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using System.ComponentModel.DataAnnotations;
 using RpgAssistant.Domain.Exceptions;
 using ValidationException = RpgAssistant.Domain.Exceptions.ValidationException;
@@ -5,9 +6,8 @@ using ValidationException = RpgAssistant.Domain.Exceptions.ValidationException;
 namespace RpgAssistant.Domain.Models;
 
 public record BaseValueObject
-{
+{ 
     protected virtual string ModelName { get; set; } = nameof(BaseValueObject);
-    protected readonly IList<ValidationMessage> ValidationMessages = new List<ValidationMessage>();
 
     protected virtual void Validate()
     {
@@ -19,17 +19,17 @@ public record BaseValueObject
         
         var context = new ValidationContext(this, serviceProvider: null, items: null);
         var validationResults = new List<ValidationResult>();
-        
-        if (!Validator.TryValidateObject(this, context, validationResults, validateAllProperties: true))
-        {
+
+        if (Validator.TryValidateObject(this, context, validationResults, validateAllProperties: true)) 
+            return;
+
+        var validationMessages = validationResults
+            .Select(x => new ValidationMessage(x.MemberNames.First(), x.ErrorMessage!))
+            .ToImmutableList();
             
-        }
-        
         throw new ValidationException(
             "Validation Error.",
-            "Model properties are not valid.",
-            ValidationMessages);
+            $"Model {ModelName} properties are not valid.",
+            validationMessages);
     }
-    
-    protected void ValidateProperty
 }
