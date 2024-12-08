bb2fe2cc692b0d6dbe0ed4125729477c62514dc8
exoil <jakubtederko@outlook.com>
2024-12-08 15:42:30 +0100

    Update

---
+++ src/RpgAssistant.Application/Handlers/CommandHandlers/DeleteKnowsCharacterCommandHandler.cs
@@ -0,0 +1,38 @@
+using System;
+using MediatR;
+using RpgAssistant.Application.Handlers.CommandHandlers.Commands;
+using RpgAssistant.Application.Models;
+using RpgAssistant.Application.Utilities;
+using RpgAssistant.Domain.Extensions;
+using RpgAssistant.Infrastructure.IRepositories;
+
+namespace RpgAssistant.Application.Handlers.CommandHandlers;
+
+public class DeleteKnowsCharacterCommandHandler
+    : IRequestHandler<DeleteKnowsCharacterCommand, Result<Exception>>
+{
+    private readonly ICharacterRepository _characterRepository;
+
+    public DeleteKnowsCharacterCommandHandler(ICharacterRepository characterRepository)
+    {
+        _characterRepository = characterRepository;
+    }
+
+    public async Task<Result<Exception>> Handle(DeleteKnowsCharacterCommand request, CancellationToken cancellationToken)
+    {
+
+        try
+        {
+            await _characterRepository.DeleteKnowsRelationAsync(
+                request.SourceId.ToUlidFormat(),
+                request.TargetId.ToUlidFormat(),
+                cancellationToken);
+        }
+        catch (Exception e)
+        {
+            return ExcptionUtility.ResolveExceptionToReturn(e);
+        }
+
+        return new Result<Exception>();
+    }
+}
