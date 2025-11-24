876d524f1b028c7bcd5563a49e443201d2cbd115
Jakub Tederko <jakubtederko@outlook.com>
2025-11-24 20:55:27 +0100

    Updates

--- src/models/CharacterNode.ts
+++ src/models/CharacterNode.ts
@@ -1,11 +1,14 @@
+import type { Character } from '@/services/Models/Character'
 import type { NodeWithId } from 'v-network-graph'
 
 export class CharacterNode implements NodeWithId {
+  characterData: Character
   id: string
   name: string
 
-  constructor(id: string, name: string) {
-    this.id = id
-    this.name = name
+  constructor(character: Character) {
+    this.characterData = character
+    this.id = character.id
+    this.name = character.name
   }
 }
