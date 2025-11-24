a7a52cf0f476bc85fb16a401eed2eb572c028bac
Jakub Tederko <jakubtederko@outlook.com>
2025-11-24 20:38:01 +0100

    Update

---
+++ src/services/Models/Character.ts
@@ -0,0 +1,11 @@
+export class Character {
+  public id: string
+  public name: string
+  public version: number
+
+  constructor(id: string, name: string, version: number) {
+    this.id = id
+    this.name = name
+    this.version = version
+  }
+}
