1c4f9c63de007ff140cdb093fa69174c216dfed7
Jakub Tederko <jakubtederko@outlook.com>
2026-06-16 07:57:02 +0200

    Update unit tests

--- test/LoreWeave.Application.Test/Commands/CommandHandlers/CreateKnowRelationCommandHandlerTest.cs
+++ test/LoreWeave.Application.Test/Commands/CommandHandlers/CreateKnowRelationCommandHandlerTest.cs
@@ -40,7 +40,7 @@ public class CreateKnowRelationCommandHandlerTest
     public async Task InvokeAsync_WhenBothCharactersExist_ReturnsUlid()
     {
         // Arrange
-        var command = new CreateKnowRelationCommand(_fromCharacterId, _toCharacterId, "They know each other");
+        var command = new CreateKnowRelationCommand(_fromCharacterId, _toCharacterId, "They know each other", true);
         _characterRepository
             .ExistsAsync(Arg.Any<IAsyncTransaction>(), _fromCharacterId)
             .Returns((true, 1));
@@ -55,6 +55,36 @@ public class CreateKnowRelationCommandHandlerTest
         result.IsSuccess.ShouldBeTrue("Know relation creation should succeed when both characters exist");
         result.Value.ShouldNotBe(default(Ulid), "Returned ULID should not be empty");
         await _transaction.Received(1).CommitAsync();
+        await _characterRepository
+            .Received(1)
+            .CreateKnowRelationAsync(
+                Arg.Any<IAsyncTransaction>(),
+                Arg.Is<LoreWeave.Domain.Entities.Knows.Commands.CreateKnowRelation>(r => r.IsStrongRelation));
+    }
+
+    [Fact]
+    [Trait(Constants.TraitName, Constants.TestTitle)]
+    public async Task InvokeAsync_WhenRelationIsNotStrong_PassesIsStrongRelationFalseToRepository()
+    {
+        // Arrange
+        var command = new CreateKnowRelationCommand(_fromCharacterId, _toCharacterId, "They know each other", false);
+        _characterRepository
+            .ExistsAsync(Arg.Any<IAsyncTransaction>(), _fromCharacterId)
+            .Returns((true, 1));
+        _characterRepository
+            .ExistsAsync(Arg.Any<IAsyncTransaction>(), _toCharacterId)
+            .Returns((true, 1));
+
+        // Act
+        var result = await _sut.InvokeAsync(command);
+
+        // Assert
+        result.IsSuccess.ShouldBeTrue("Know relation creation should succeed when both characters exist");
+        await _characterRepository
+            .Received(1)
+            .CreateKnowRelationAsync(
+                Arg.Any<IAsyncTransaction>(),
+                Arg.Is<LoreWeave.Domain.Entities.Knows.Commands.CreateKnowRelation>(r => !r.IsStrongRelation));
     }
 
     [Fact]
@@ -62,7 +92,7 @@ public class CreateKnowRelationCommandHandlerTest
     public async Task InvokeAsync_WhenFromCharacterDoesNotExist_ReturnsUnprocessableContentException()
     {
         // Arrange
-        var command = new CreateKnowRelationCommand(_fromCharacterId, _toCharacterId, "They know each other");
+        var command = new CreateKnowRelationCommand(_fromCharacterId, _toCharacterId, "They know each other", true);
         _characterRepository
             .ExistsAsync(Arg.Any<IAsyncTransaction>(), _fromCharacterId)
             .Returns((false, 0));
@@ -80,7 +110,7 @@ public class CreateKnowRelationCommandHandlerTest
     public async Task InvokeAsync_WhenToCharacterDoesNotExist_ReturnsUnprocessableContentException()
     {
         // Arrange
-        var command = new CreateKnowRelationCommand(_fromCharacterId, _toCharacterId, "They know each other");
+        var command = new CreateKnowRelationCommand(_fromCharacterId, _toCharacterId, "They know each other", true);
         _characterRepository
             .ExistsAsync(Arg.Any<IAsyncTransaction>(), _fromCharacterId)
             .Returns((true, 1));
@@ -101,7 +131,7 @@ public class CreateKnowRelationCommandHandlerTest
     public async Task InvokeAsync_WhenRepositoryThrows_ReturnsExceptionAndRollsBack()
     {
         // Arrange
-        var command = new CreateKnowRelationCommand(_fromCharacterId, _toCharacterId, "They know each other");
+        var command = new CreateKnowRelationCommand(_fromCharacterId, _toCharacterId, "They know each other", true);
         var expectedException = new Exception("DB error");
         _characterRepository
             .ExistsAsync(Arg.Any<IAsyncTransaction>(), _fromCharacterId)
