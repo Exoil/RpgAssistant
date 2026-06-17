ca63a5cfcc40a61d40a2bc16d1da367568586a52
Jakub Tederko <jakubtederko@outlook.com>
2026-06-17 09:04:05 +0200

    apply improvments

--- src/LoreWeave.Domain/Models/CharacterWithKnowRelation.cs
+++ src/LoreWeave.Domain/Models/CharacterWithKnowRelation.cs
@@ -4,8 +4,3 @@ public sealed record CharacterWithKnowRelation(
     Ulid Id,
     string Name,
     IReadOnlyCollection<KnowRelationDetail> KnowRelations);
-
-public sealed record KnowRelationDetail(
-    Ulid CharacterId,
-    string Description,
-    bool IsStrongRelation);
