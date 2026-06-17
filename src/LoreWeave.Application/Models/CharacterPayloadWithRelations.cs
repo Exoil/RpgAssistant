fc33e8cd3002612b39651d1289b673e3a64ca0dd
Jakub Tederko <jakubtederko@outlook.com>
2026-06-17 09:02:41 +0200

    create KnowCharacterRelationPayload cs file

--- src/LoreWeave.Application/Models/CharacterPayloadWithRelations.cs
+++ src/LoreWeave.Application/Models/CharacterPayloadWithRelations.cs
@@ -11,12 +11,3 @@ public record CharacterPayloadWithRelations(
     string Name,
     [property: JsonPropertyName("knowCharacters")]
     IReadOnlyCollection<KnowCharacterRelationPayload> KnowCharacters);
-
-public record KnowCharacterRelationPayload(
-    [property: JsonPropertyName("characterId")]
-    Guid CharacterId,
-    [StringLength(256, MinimumLength = 0, ErrorMessage = "Value for {0} must be between {1} and {2} characters.")]
-    [property: JsonPropertyName("description")]
-    string Description,
-    [property: JsonPropertyName("isStrongRelation")]
-    bool IsStrongRelation);
